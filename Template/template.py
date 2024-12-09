251276a8ca7f313bdec8c08b1ce585617829cddb
pougetb <bastian.pouget@gmail.com>
2024-12-09 18:45:20 +0100

    maj template

--- Template/template.py
+++ Template/template.py
@@ -9,17 +9,17 @@ def solve_part2(puzzle_input: list):
 
 if __name__ == "__main__":
     print("----- Example 1 -----")
-    example1_input = open("example1.txt", "r").read().splitlines()
+    example1_input = open("example.txt", "r").read().splitlines()
     print(f"Example 1 result : {solve_part1(example1_input)}\n")
 
     print("----- Part 1 -----")
-    part1_input = open("part1.txt", "r").read().splitlines()
+    part1_input = open("input.txt", "r").read().splitlines()
     print(f"Part 1 result : {solve_part1(part1_input)}\n")
 
     print("----- Example 2 -----")
-    example2_input = open("example2.txt", "r").read().splitlines()
+    example2_input = open("example.txt", "r").read().splitlines()
     print(f"Example 2 result : {solve_part2(example2_input)}\n")
     
     print("----- Part 2 -----")
-    part2_input = open("part2.txt", "r").read().splitlines()
+    part2_input = open("input.txt", "r").read().splitlines()
     print(f"Part 2 result : {solve_part2(part2_input)}\n")
